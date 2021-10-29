ac7eea2984a5185b2db46956fde64edb994fb5b0
lucasr22 <91099098+lucasr22@users.noreply.github.com>
2021-10-28 21:11:37 -0300

    Add files via upload

---
+++ projetofinal/app.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/PROJETOFINAL/style.css">
+</head>
+<body>
+
+<section>
+<h1 id="titulo">diploma eteot: </h1>
+<form >
+    <input type="email" name="email" placeholder="email">
+    <input type="password" name="senha" placeholder="senha">
+    <button type="submit">enviar</button>
+</form>
+</section>
+
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
---
+++ projetofinal/area_diploma.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/PROJETOFINAL/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <section>
+    <h1> cadastro para seu diploma:</h1>
+    <hr>
+    <form action="">
+    <input type="text" name="nome" placeholder="Nome">
+        <input type="email" name="email" placeholder="Email">
+            <input type="password" name="senha" placeholder="Senha">
+            <select name="escola" id="cars">
+                <option value="volvo">Ensino fundamental</option>
+                    <option value="saab">Ensino médio completo</option>
+                                <option value="mercedes">Mercedes</option>
+                    <option value="audi">Audi</option>
+</select>
+<h2>Sexo:</h2>
+<label for="feminino">feminino</label>
+<input type="radio" id="feminino" name="sexo" value="feminino">
+<label for="masculino">masculino</label>
+<input type="radio" id="masculino" name="sexo" value="JavaScript">
+    </form>
+
+    
+</body>
+</html>
